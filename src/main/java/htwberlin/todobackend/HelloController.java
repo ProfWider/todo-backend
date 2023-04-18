f0d64d2138500697e1bb7bd5bf749e79d779d883
Prof. Dr. Arif Wider <wider@htw-berlin.de>
2023-04-18 12:12:28 +0200

    more controller routes

--- src/main/java/htwberlin/todobackend/HelloController.java
+++ src/main/java/htwberlin/todobackend/HelloController.java
@@ -1,14 +1,31 @@
 package htwberlin.todobackend;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
-    @GetMapping("/greeting")
+    @GetMapping("/greeting/welcome/")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name) {
         return "hello, " + name;
+    }
+
+    @GetMapping("/path/param/{name}/{par2}")
+    public String pathparam(
+            @PathVariable(name="name") String nam,
+            @PathVariable(name="par2") String par2) {
+        return "hello, " + nam+par2;
+    }
+
+    @GetMapping("/path/param")
+    public String pathparam(
+            @RequestHeader(value="User-Agent") String par2) {
+        return "hello, " + par2;
+    }
+
+    @PostMapping("/send")
+    public String postparam() {
+        return "hello, ";
     }
 
     @GetMapping("/")
