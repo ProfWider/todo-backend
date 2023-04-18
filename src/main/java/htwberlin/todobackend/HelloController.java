81a10688007a843dbfb94ee12b04be81244f19c4
Prof. Dr. Arif Wider <wider@htw-berlin.de>
2023-04-18 14:00:12 +0200

    french greeting

--- src/main/java/htwberlin/todobackend/HelloController.java
+++ src/main/java/htwberlin/todobackend/HelloController.java
@@ -24,7 +24,7 @@ public class HelloController {
 
     @PostMapping("/postexample")
     public String postparam() {
-        return "hello, ";
+        return "bonjour, ";
     }
 
     @GetMapping("/")
